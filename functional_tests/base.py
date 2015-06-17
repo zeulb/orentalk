359f34b0f4f17fdeedda396a370bf0e3c6b487bd
Stefano Chiesa <base278@gmail.com>
2015-06-16 20:27:09 -0500

    Add first functional test

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,12 +1,13 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
 CHROME_DRIVER_LOCATION = '/Users/zeulb/Dropbox/Programming/TDDPython/chromedriver'
 
-class FunctionalTest(LiveServerTestCase):
+class FunctionalTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome(CHROME_DRIVER_LOCATION)
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
--- functional_tests/test_homepage.py
+++ functional_tests/test_homepage.py
@@ -2,5 +2,14 @@ from .base import FunctionalTest
 
 class TestHomePage(FunctionalTest):
 
-	def test_create_chat_room(self):
+	def test_redirected_when_creating_new_chat_room(self):
 		self.browser.get(self.live_server_url)
+
+		current_url = self.browser.current_url
+		
+		new_chat_form = self.browser.find_element_by_id('new-chat')
+		new_chat_form.send_keys('Gajah terbang\n')
+
+		new_url = self.browser.current_url
+
+		self.assertNotEqual(current_url, new_url)
