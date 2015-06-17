1a72a472a9853b280657174395b6aaa379847ab8
Stefano Chiesa <base278@gmail.com>
2015-06-16 23:51:31 -0500

    Add unit test for new chat room form

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from chat.models import Room
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+	def test_can_create_new_room(self):
+		self.client.post(
+			'/talk/newroom',
+			data={'title': 'Budi anduk'}
+		)
+
+		self.assertEqual(Room.objects.count(), 1)
+		new_room = Room.objects.first()
+		self.assertEqual(new_room.title, 'Budi anduk')
+
